c1e7ad9f8da30482702fab64e171dde12f2356f5
Tejashwini S Uranakar <winisuranakar@gmail.com>
2024-12-19 22:57:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("submit").addEventListener("click", function () {
     const passcode = document.getElementById("passcode").value;
     const accessGrantedAudio = new Audio("accessgranted.mp3");
     const accessDeniedAudio = new Audio("accessdenied.mp3");
-    const validPasscodes = ["wx3nf0", "ax3p6h", "1zp5qm", "ld1k0o", "k0m1sv"]; // Replace with your valid passcodes
+    const validPasscodes = ["wx3nf0", "ax3p6h", "1zp5qm", "ld1k0o", "k0m1sv"]; 
     const maxAttempts = 3;
     let attempts = parseInt(localStorage.getItem("attempts") || "0");
 
@@ -16,18 +16,15 @@ document.getElementById("submit").addEventListener("click", function () {
         alert("Access Granted!");
         accessGrantedAudio.play();
         
-        // Redirect after 5 seconds
         setTimeout(() => {
-            window.location.href = "C:\\Users\\nitis\\Desktop\\MissionSpectrumWebsite\\circuit.html";
-        }, 5000); // 5000 milliseconds = 5 seconds
+            window.location.href = "circuit.html";
+        }, 5000);
 
     } else {
         attempts++;
         localStorage.setItem("attempts", attempts.toString());
         alert(`Access Denied! Attempts left: ${maxAttempts - attempts}`);
         accessDeniedAudio.play();
-
-        // If attempts reach max, reset after alert
         if (attempts >= maxAttempts) {
             localStorage.removeItem("attempts");
         }
